47c883921f063a23c01437f4f30294586ac376d0
Abdallah <rababahabdallah00@gmail.com>
2023-07-26 16:57:07 +0300

    ss

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<UserService>();
-
+builder.Services.AddCors();
 
 
 builder.Services.AddControllers();
@@ -24,7 +24,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200") );
 
  
 app.MapControllers();
